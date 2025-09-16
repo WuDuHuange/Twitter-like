0171d09c8c58e66757a41324ffc792da66cde5a1
Rothbart <1029580076@qq.com>
2025-09-16 20:19:42 +0800

    本地配置（zmz）

---
+++ frontend/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  transpileDependencies: [],
+
+  devServer: {
+    port: 8080,
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
+};
